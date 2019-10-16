35e2334efb2dad1c58fe4c558d07b1f17be6772d
Hyakuya <limpan.naslund@gmail.com>
2019-10-16 21:09:08 +0200

    Ninja added

--- script.js
+++ script.js
@@ -36,7 +36,7 @@ async function start() {
 }
 
 function loadLabeledImages() {
-  const labels = ['Beasttrollmc', 'Asmongold', 'Sodapoppin', 'Pink sparkles', 'Esfand']
+  const labels = ['Beasttrollmc', 'Asmongold', 'Sodapoppin', 'Pink sparkles', 'Esfand', 'Ninja']
   return Promise.all(
     labels.map(async label => {
       const descriptions = []
